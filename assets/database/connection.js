1f67328645966e81bca1f65a9a6f7e330c03a376
Sandi Pratama <yue_xie@outlook.com>
2020-11-23 19:42:36 +0700

    update

---
+++ assets/database/connection.js
@@ -0,0 +1,13 @@
+// Initialize Firebase
+var firebaseConfig = {
+    apiKey: "AIzaSyAxGjF-RVsjZc77P4u_HaW-XlDlKa5gTP4",
+    authDomain: "test-login-dacfb.firebaseapp.com",
+    databaseURL: "https://test-login-dacfb.firebaseio.com",
+    projectId: "test-login-dacfb",
+    storageBucket: "test-login-dacfb.appspot.com",
+    messagingSenderId: "956610046968",
+    appId: "1:956610046968:web:0ec187b189cfe7ea8d040d"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
